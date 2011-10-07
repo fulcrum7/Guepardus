1e9a8805c564303cefb5e0467e8080f9fe32937d
roman <roman@localhost.localdomain>
2011-10-07 17:34:28 +0400

    Defered init

---
+++ tools/defered_init/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+void call_privileged();
+
+int main()
+{
+	pid_t pid;	
+	/* do the fork() to give control to our privileged application */
+	/* TODO: vfork is better */	
+	pid=fork();	
+	switch(pid)
+	{
+		case -1: /* fork error, should call plan B mode */ 
+				break;
+		case 0:  /* child process, call privileged application */
+				call_privileged();
+				break;
+		default: 
+				break;
+
+	}
+	
+	/*
+	 *  this process should be SystemV init, but before 	run init
+	 *  we should do the deffered_init_script to mount normal rootfs
+     *  or maybe for smth else
+	 */
+
+	/*   do the script */
+	system("./defered_init_script");
+    /*   ready to start init */
+	printf("This is process %d\nStarting the init...\n",getpid());
+	/* for debugging */	
+	sleep(15);
+
+	/*  change  the image of this process */
+	execl("/sbin/init","init");		
+
+}
+
+
+void call_privileged()
+{
+	/* this is child process */
+	printf("Privileged application pid %d\n",getpid());
+	/* change the image of this process to priv_app */
+    //execl("/priv_app","priv_app");
+	exit(EXIT_SUCCESS);
+}
